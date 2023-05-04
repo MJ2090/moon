bf02cf233d70d6faeb03108aeac3ea1363670af6
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-04 15:28:43 -0700

    s

--- setup.sh
+++ setup.sh
@@ -4,6 +4,6 @@ echo "ALL started ================================="
 sudo apt install python3-pip -y
 pip install django
 pip install gunicorn
-sudo apt install nginx
+sudo apt install nginx -y
 
 echo "ALL ended ================================="
