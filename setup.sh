88e0739977180a53e7f3d66bf37d67f4eac4c6c5
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-04 17:17:31 -0700

    s

--- setup.sh
+++ setup.sh
@@ -6,4 +6,11 @@ pip install django
 pip install gunicorn
 sudo apt install nginx -y
 
+echo 'alias gl="git pull"' >> ~/.bashrc
+echo 'alias dr="python3 manage.py runserver 0.0.0.0:8000"' >> ~/.bashrc
+echo 'alias p3="python3"' >> ~/.bashrc
+echo 'alias dc="python3 manage.py collectstatic"' >> ~/.bashrc
+echo 'alias gk="pkill gunicorn"' >> ~/.bashrc
+echo 'alias gs="gunicorn -c config/gunicorn/prod.py"' >> ~/.bashrc
+
 echo "ALL ended ================================="
