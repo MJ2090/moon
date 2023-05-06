4a2d04cb6eb1c588b9aa1896b2219aac9cb435e6
MJ2090 <125602296+MJ2090@users.noreply.github.com>
2023-05-05 17:28:43 -0700

    s

--- endpoint/views.py
+++ endpoint/views.py
@@ -3,21 +3,16 @@ from django.shortcuts import render
 import json
 from django.conf import settings
 
-# Create your views here.
-
-print("in the view!!")
-x=0
 
 def test_async(request):
     if request.method == 'POST':
-        ai_message = 'wwwww'
-        prompt_template = """Below is aninstruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+        prompt_template = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
 
 ### Instruction:
 Below is a dialogue between a patient and a therapist. Write one reply as if you were the therapist.
 
 ### Input:"""
-        print("messages , ", request.POST['messages'])
+        print("POST messages : ", request.POST['messages'])
         prompt = prompt_template
         messages = json.loads(request.POST['messages'])
         for item in messages:
@@ -31,9 +26,9 @@ Therapist: {item['content']}"""
 
 ### Response:
 """
-        print("prompt = ", prompt)
+        print(f"Prompt =============================\n{prompt}\n=============================")
         ai_message = settings.LLAMA.evaluate(prompt = prompt)
-        print("output = ", ai_message)
+        print(f"Output =============================\n{ai_message}\n=============================")
         splitted = ai_message.split("### Response:")
         if len(splitted)>1:
             ans = splitted[1]
